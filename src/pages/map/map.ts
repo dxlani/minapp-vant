48c8a95568538ba082e1b889f22dd7f70e683fc1
dxlani <870188670@qq.com>
2018-10-05 12:02:13 +0800

    minapp初始化

---
+++ src/pages/map/map.ts
@@ -0,0 +1,56 @@
+// 此文件是由模板文件 ".dtpl/page/$rawModuleName.ts.dtpl" 生成的，你可以自行修改模板
+
+import {pagify, MyPage, wxp} from 'base/'
+
+@pagify()
+export default class extends MyPage {
+  data = {
+    markers: [{
+      iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color:"#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  }
+     
+      
+      
+  
+  async onLoad(options: any) {
+    console.log(await wxp.getUserInfo())
+  }
+  async regionchange(e:any) {
+    console.log(e.type)
+  }
+  markertap(e:any) {
+    console.log(e.markerId)
+  }
+  controltap(e:any) {
+    console.log(e.controlId)
+  }
+}
