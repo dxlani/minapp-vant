fa2b923de3290b5626832a521fc03adcd5e1f126
dxlani <870188670@qq.com>
2018-10-14 12:49:50 +0800

    表单组件完结

--- src/pages/vant-table/vant-table.ts
+++ src/pages/vant-table/vant-table.ts
@@ -6,15 +6,45 @@ import {pagify, MyPage} from 'base/'
 export default class extends MyPage {
   data = {
     list: ['a', 'b', 'c'],
-    result: ['a', 'b']
+    result: ['a', 'b'],
+    checked: true,
+    icon: {
+      normal: '../../images/heart@3x.png',
+      active: '../../images/heart1@3x.png'
+    },
+    username:'',
+    password:'',
+    message:'',
+    sms:'',
+    value: '',
+    radio: '1',
   }
 
   async onLoad(options: any) {
     // console.log(await wxp.getUserInfo())
   }
   onChange(event) {
+    console.log(event.detail);
     this.setData({
       result: event.detail
     });
   }
+  onChange1(event) {
+    console.log(event.detail);
+  }
+  onChange2(event) {
+    wx.showModal({
+      title: '提示',
+      content: '是否切换开关？',
+      success: res => {
+        if (res.confirm) {
+          console.log(event.detail);
+          this.setData({ checked: event.detail});
+        }
+      }
+    });
+   
+  }
+
+
 }
